6d559c68a79f05895c3fc9dcef9c73b78d8aa9dd
ABHIJITH T <hey.abhijith@gmail.com>
2025-08-16 20:00:11 +0530

    Create day-1.rs

---
+++ day-1.rs
@@ -0,0 +1,14 @@
+fn test() {
+    println!("Hello its test file ");
+}
+
+fn main() {
+    println!("Lets get Rusty");
+    test();
+    test_two();
+}
+
+
+fn test_two(){
+	println!("ITs test 2 ");
+}
